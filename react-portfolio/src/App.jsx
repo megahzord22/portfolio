72974b8ecfd7b14cf1a20ab374b1f9268e974d31
Megan Grant <granmega@oregonstate.edu>
2024-08-11 22:41:43 -0700

    updating portfolio with structure

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,8 +1,11 @@
-import styles from './App.modules.css';
+import styles from './App.module.css';
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
 
-  return <div className={styles.App}>Hello world</div>;
+  return <div className={styles.App}>
+    <Navbar/>
+  </div>;
   
 }
 
